734ff17b904aed7c3e774d90568ac3048d9331a8
yyyyyym <promiyou66@gmail.com>
2021-06-29 16:12:16 +0900

    # WARNING: head commit changed in the meantime
    test

--- src/main/java/com/north/bi/HomeController.java
+++ src/main/java/com/north/bi/HomeController.java
@@ -4,17 +4,11 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import javax.jws.WebParam.Mode;
 
-import org.apache.taglibs.standard.lang.jstl.test.beans.PublicBean1;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.Log4jConfigurer;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
